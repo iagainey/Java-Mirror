6b41b6d06c060c9a2587cab9ab3accfc137468f8
iagainey <iagainey7197@eagle.fgcu.edu>
2018-06-01 20:58:39 -0400

    MemberWrap - added getSignature()

--- src/main/java/org/iag/utility/mirror/MemberWrap.java
+++ src/main/java/org/iag/utility/mirror/MemberWrap.java
@@ -10,6 +10,7 @@ import java.lang.reflect.GenericDeclaration;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.TypeVariable;
 import java.util.Collection;
 import java.util.Objects;
@@ -557,7 +558,19 @@ public class MemberWrap< C,
 
 	/**
 	 * 
+	 * @return a {@code new String} that attempts to copy the signature of the
+	 *         {@link member}. If {@link member} is {@code null} then returns
+	 *         {@code "null"}
 	 */
+	public @NonNull String
+		   getSignature(){
+		return isMember() ? String.format( "%s %s%s",
+										   Modifier.toString( getModifiers() ),
+										   getName(),
+										   isExecutable() ? String.format( "(%s)",
+																		   (Object[]) getParameterTypes() )
+														  : "" )
+						  : "null";
 	}
 
 	/**
